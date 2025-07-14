9934cbec8530eb4f729e14c32eea6fcc6b4840ae
nonfan <nonfan@users.noreply.github.com>
2025-07-14 07:35:41 +0000

    deploy: 55e2befbd33ce83aafa4df2c666bca6cf5339dfc

--- src/layout/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from "react-router-dom";
-import routes from "@/routes";
-
-function Layout() {
-  const element = useRoutes(routes);
-  return <>{element}</>; // 或者 return <div>{element}</div>;
-}
-
-export default Layout;
